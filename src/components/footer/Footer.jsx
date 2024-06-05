3bfe7640f0ca9cdbaf591c3f5effc741b7477531
OdashevBexruzbek <bexruzbekodashev@gmail.com>
2024-06-05 14:06:48 +0500

    users

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>Copyright © 2024 - All right reserved by <a href="https://t.me/xack_devs">Bexruzbek</a> Industries Ltd.</p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
